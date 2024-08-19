ebe3a4f19944b0e71356adfe00e7a36010ae1cc9
Marvin Koball <koball@hokona.de>
2024-08-19 18:54:28 +0200

    storage checker

--- questions.js
+++ questions.js
@@ -1,1 +1,8 @@
-console.log('hi from external');
+const root = localStorage.getItem('root');
+const storageStatus = document.getElementById("storage-satus");
+if (root && storageStatus) {
+    storageStatus.textContent = "there is a file that was parsed";
+} else {
+
+    storageStatus.textContent = "no file was parsed"
+}
