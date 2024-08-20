5005f2b31c2a6ab6f5fa8aba615d8548f6a88837
Marvin Koball <koball@hokona.de>
2024-08-20 20:28:34 +0200

    added multi seleled and refactored state

--- questions.js
+++ questions.js
@@ -1,6 +1,69 @@
-let currentQuestionIndex = localStorage.getItem('currentQuestionIndex') ?? 0;
-let correctAnswers = localStorage.getItem('correctAnswers') ?? 0;
 let questions = [];
+const State = {
+    _currentQuestionIndex: 0,
+    _correctAnswers: 0,
+    init() {
+        this._currentQuestionIndex = Number(localStorage.getItem('currentQuestionIndex')) ?? 0;
+        this._correctAnswers = Number(localStorage.getItem('correctAnswers')) ?? 0;
+
+    },
+    get currentQuestionIndex() {
+        return this._currentQuestionIndex;
+    },
+    set currentQuestionIndex(value) {
+        this._currentQuestionIndex = value;
+        this._saveToLocalStorage('currentQuestionIndex', value);
+        if (document.getElementById('counter')) {
+            document.getElementById('counter').textContent = `${this._currentQuestionIndex}/${questions.length}`
+        }
+
+    },
+    get correctAnswers() {
+        return this._correctAnswers;
+    },
+    set correctAnswers(value) {
+        this._correctAnswers = value;
+        this._saveToLocalStorage('correctAnswers', value);
+    },
+    _saveToLocalStorage(key, value) {
+        localStorage.setItem(key, value);
+    }
+};
+
+State.init();
+
+function uploadFile() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    if (!file) {
+        document.getElementById('file-status').textContent = "Please select a file.";
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        try {
+            const json = JSON.parse(event.target.result)
+            json.questions = json.questions
+                .map(value => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value);
+            localStorage.setItem('root', JSON.stringify(json));
+            document.getElementById('file-status').textContent = "JSON file parsed and stored in local storage.";
+        } catch (e) {
+            document.getElementById('file-status').textContent = "Error parsing JSON file.";
+        }
+    };
+
+    reader.onerror = function() {
+        document.getElementById('file-status').textContent = "Error reading file.";
+    };
+
+    reader.readAsText(file);
+    loadQuestions()
+    State.currentQuestionIndex = 0;
+    State.correctAnswers = 0;
+}
+
 
 document.getElementById('submit').addEventListener('click', checkAnswer);
 document.getElementById('next').addEventListener('click', loadNextQuestion);
@@ -8,7 +71,7 @@ document.getElementById('next').addEventListener('click', loadNextQuestion);
 function loadQuestions() {
     const data = JSON.parse(localStorage.getItem('root'));
     if (data?.questions) {
-        questions = data.questions; // Load questions from local storage
+        questions = data.questions
         loadNextQuestion();
     }
 }
@@ -16,8 +79,8 @@ function loadQuestions() {
 function loadNextQuestion() {
     document.getElementById('feedback').textContent = '';
     document.getElementById('next').style.display = 'none';
-    if (currentQuestionIndex < questions.length) {
-        const question = questions[currentQuestionIndex];
+    if (State.currentQuestionIndex < questions.length) {
+        const question = questions[State.currentQuestionIndex];
         displayQuestion(question);
     } else {
         showResults();
@@ -25,25 +88,55 @@ function loadNextQuestion() {
 }
 
 function displayQuestion(question) {
+    if (question?.type === "true_or_false") {
+        displayTrueOrFalse(question);
+    } else if (question?.type === "multi_select") {
+        displayMultiSelect(question)
+    }
+}
+
+function displayMultiSelect(question) {
+    document.getElementById('question').textContent = question.header_text;
+    const optionsContainer = document.getElementById('options');
+    optionsContainer.innerHTML = ''; // Clear previous options
+    question.options.forEach((element, index) => {
+        const option = document.createElement('li');
+        option.textContent = element.text
+        option.setAttribute("index", index)
+        option.addEventListener('click', () => selectMultiSelect(index));
+        optionsContainer.appendChild(option);
+    })
+}
+function selectMultiSelect(index) {
+    const options = document.querySelectorAll('#options li');
+    Array.from(options).forEach((option) => {
+        if (option.getAttribute("index") === index + '') {
+            option.classList.toggle('selected')
+            option.toggleAttribute('selected')
+        }
+
+    })
+}
+
+function displayTrueOrFalse(question) {
     document.getElementById('question').textContent = question.text;
     const optionsContainer = document.getElementById('options');
     optionsContainer.innerHTML = ''; // Clear previous options
-    // As it's true or false, create only two options
     const trueOption = document.createElement('li');
     trueOption.textContent = 'Wahr';
-    trueOption.addEventListener('click', () => selectOption(true));
+    trueOption.addEventListener('click', () => selectTrueOrFalse(true));
     optionsContainer.appendChild(trueOption);
 
     const falseOption = document.createElement('li');
     falseOption.textContent = 'Falsch';
-    falseOption.addEventListener('click', () => selectOption(false));
+    falseOption.addEventListener('click', () => selectTrueOrFalse(false));
     optionsContainer.appendChild(falseOption);
 }
 
-function selectOption(selectedValue) {
+function selectTrueOrFalse(selectedValue) {
     const options = document.querySelectorAll('#options li');
     options.forEach(option => {
-        option.classList.remove('selected'); // Remove selected from all options
+        option.classList.remove('selected');
     });
     const selectedOption = Array.from(options).find(option => option.textContent === (selectedValue ? 'Wahr' : 'Falsch'));
     selectedOption.classList.add('selected');
@@ -54,38 +147,52 @@ function checkAnswer() {
     if (feedbackSet?.textContent !== '') {
         return;
     }
-    const selectedOption = document.querySelector('#options li.selected');
-    const isCorrect = (selectedOption.textContent === 'Wahr') === questions[currentQuestionIndex].correct;
+    const currentQuestion = questions[State.currentQuestionIndex];
+    let isCorrect = false;
+    if (currentQuestion.type === 'true_or_false') {
+        isCorrect = checkTrueOrFalse(currentQuestion);
+    } else if (currentQuestion.type === 'multi_select') {
+        isCorrect = checkMultiSelect(currentQuestion)
+    }
     if (isCorrect) {
-        incrementCorrectAnswer()
+        State.correctAnswers += 1;
         document.getElementById('feedback').textContent = 'Richtig!';
-        document.getElementById('feedback').textContent += ` Hinweis: ${questions[currentQuestionIndex].hint}`;
     } else {
         document.getElementById('feedback').textContent = 'Falsch!';
-        document.getElementById('feedback').textContent += ` Hinweis: ${questions[currentQuestionIndex].hint}`;
     }
-    incrementQuestionIndex()
+    if (currentQuestion?.hint) {
+        document.getElementById('feedback').textContent += ` Hinweis: ${currentQuestion.hint}`;
+    }
+    State.currentQuestionIndex += 1;
     document.getElementById('next').style.display = 'block';
 }
-function incrementQuestionIndex() {
-    localStorage.setItem('currentQuestionIndex', ++currentQuestionIndex);
-    document.getElementById('counter').textContent = `${currentQuestionIndex}/${questions.length}`
+
+function checkMultiSelect(currentQuestion) {
+    const options = document.querySelectorAll('#options li');
+    let isCorrect = true;
+    options.forEach((option) => {
+        currentQuestion.options.forEach((element, index) => {
+            if (index + '' === option.getAttribute('index') && !(option.hasAttribute('selected') === element.correct)) {
+                isCorrect = false;
+            }
+        })
+    })
+    return isCorrect;
 }
-function incrementCorrectAnswer() {
-    localStorage.setItem('correctAnswers', ++correctAnswers);
+function checkTrueOrFalse(currentQuestion) {
+    const selectedOption = document.querySelector('#options li.selected');
+    return (selectedOption.textContent === 'Wahr') === currentQuestion.correct;
 }
 
 function showResults() {
-    const quizContainer = document.getElementById('quiz-container');
-    quizContainer.innerHTML = `<h1>Quiz Abgeschlossen</h1>
-    <p>Deine Punktzahl: ${correctAnswers}/${questions.length}</p>`;
+    document.getElementById('question').textContent = '';
+    const optionsContainer = document.getElementById('options');
+    optionsContainer.innerHTML = "Quiz Abgeschlossen"; // Clear previous options
+    document.getElementById('feedback').textContent = `Deine Punktzahl: ${State.correctAnswers}/${questions.length}`
 }
 function resetQuestions() {
-    localStorage.setItem('currentQuestionIndex', 0);
-    currentQuestionIndex = 0;
-    localStorage.setItem('correctAnswers', 0);
-    correctAnswers = 0;
+    State.currentQuestionIndex = 0;
+    State.correctAnswers = 0;
     loadNextQuestion()
 }
-
 window.onload = loadQuestions;
